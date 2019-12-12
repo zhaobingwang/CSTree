73e77e2061276a4bfa6081bf27406dee957459c4
No.8 <zhaobingwang@foxmali.com>
2019-12-12 19:53:24 +0800

    feat : BCL扩展方法-新增DateTime扩展 #3

--- src/CSTree/Extensions/BCLExtensions.DateTime.cs
+++ src/CSTree/Extensions/BCLExtensions.DateTime.cs
@@ -9,9 +9,115 @@ namespace CSTree
     /// </summary>
     public static partial class BCLExtensions
     {
+        /// <summary>
+        /// 转为中文日期
+        /// </summary>
+        /// <param name="dateTime">日期时间</param>
+        /// <returns></returns>
         public static string ToChineseDateString(this DateTime dateTime)
         {
             return dateTime.ToString("yyyy年MM月dd日");
+        }
+
+        /// <summary>
+        /// 转为中文日期
+        /// </summary>
+        /// <param name="dateTime">日期时间</param>
+        /// <returns></returns>
+        public static string ToChineseDateString(this DateTime? dateTime)
+        {
+            if (dateTime == null)
+                return string.Empty;
+            return dateTime.Value.ToString("yyyy年MM月dd日");
+            //return ToChineseDateString(dateTime.Value);
+        }
+
+        /// <summary>
+        /// 转为中文日期时间
+        /// </summary>
+        /// <param name="dateTime">日期时间</param>
+        /// <returns></returns>
+        public static string ToChineseDateTimeString(this DateTime dateTime)
+        {
+            return dateTime.ToString("yyyy年MM月dd日HH时mm分ss秒");
+        }
+
+        /// <summary>
+        /// 转为中文日期时间
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static string ToChineseDateTimeString(this DateTime? dateTime)
+        {
+            if (dateTime == null)
+                return string.Empty;
+            return dateTime.Value.ToString("yyyy年MM月dd日HH时mm分ss秒");
+            //return ToChineseDateTimeString(dateTime.Value);
+        }
+
+        /// <summary>
+        /// 转为yyyy-MM-dd格式字符串
+        /// </summary>
+        /// <param name="dateTime">时间日期</param>
+        /// <returns></returns>
+        public static string ToDateString(this DateTime dateTime)
+        {
+            return dateTime.ToString("yyyy-MM-dd");
+        }
+
+        /// <summary>
+        /// 转为yyyy-MM-dd格式字符串
+        /// </summary>
+        /// <param name="dateTime">时间日期</param>
+        /// <returns></returns>
+        public static string ToDateString(this DateTime? dateTime)
+        {
+            if (dateTime == null)
+                return string.Empty;
+            return dateTime.Value.ToString("yyyy-MM-dd");
+            //return ToDateString(dateTime.Value);    // 很容易忘记写.velue,造成死循环
+        }
+
+        /// <summary>
+        /// 转为友好的时间信息
+        /// 比如：1分钟前，3天前等
+        /// XXX:一些较复杂判断先简单处理实现，比如昨天，上周，上个月等
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <param name="now">如果为null,则取DateTime.Now</param>
+        /// <returns></returns>
+        public static string ToFriendlyDateString(this DateTime dateTime, DateTime? now = null)
+        {
+            if (now == null)
+                now = DateTime.Now;
+            var timeSince = now.Value.Subtract(dateTime);
+            if (timeSince.TotalMinutes < 1)
+                return "刚刚";
+            if (timeSince.TotalMinutes < 2)
+                return "1分钟前";
+            if (timeSince.TotalMinutes < 60)
+                return $"{timeSince.Minutes}分钟前";
+            if (timeSince.TotalMinutes < 120)
+                return "1小时前";
+            if (timeSince.TotalHours < 24)
+                return $"{timeSince.Hours}小时前";
+            if (timeSince.TotalDays == 1)
+                return "昨天";
+            if (timeSince.TotalDays < 7)
+                return $"{timeSince.Days}天前";
+            if (timeSince.TotalDays < 14)
+                return "上周";
+            if (timeSince.TotalDays < 21)
+                return "2周前";
+            if (timeSince.TotalDays < 28)
+                return "3周前";
+            if (timeSince.TotalDays < 60)
+                return "上个月前";
+            if (timeSince.TotalDays < 365)
+                return $"{Math.Floor(timeSince.TotalDays / 30)}个月前";
+            if (timeSince.TotalDays < 730)
+                return "去年";
+            return $"{Math.Floor(timeSince.TotalDays / 365)}年前";
         }
     }
 }
--- tests/CSTree.UnitTests/CSTree.UnitTests/BCLExtensions/BCLExtensions.DateTime.Tests.cs
+++ tests/CSTree.UnitTests/CSTree.UnitTests/BCLExtensions/BCLExtensions.DateTime.Tests.cs
@@ -11,7 +11,86 @@ namespace CSTree.UnitTests
         {
             var datetime = Convert.ToDateTime("2019-12-11 23:10:10");
             var expected = "2019年12月11日";
+
+            DateTime? nullDateTime1 = null;
+            DateTime? nullDateTime2 = datetime;
+
             Assert.True(datetime.ToChineseDateString() == expected);
+            Assert.True(nullDateTime1.ToChineseDateString() == string.Empty);
+            Assert.True(nullDateTime2.ToChineseDateString() == expected);
+        }
+
+        [Fact(DisplayName = "转中文日期时间格式-成功测试")]
+        public void ToChineseDateTimeStringShouldSuccess()
+        {
+            var datetime = Convert.ToDateTime("2019-12-11 23:10:10");
+            var expected = "2019年12月11日23时10分10秒";
+
+            DateTime? nullDateTime1 = null;
+            DateTime? nullDateTime2 = datetime;
+
+            Assert.True(datetime.ToChineseDateTimeString() == expected);
+            Assert.True(nullDateTime1.ToChineseDateTimeString() == string.Empty);
+            Assert.True(nullDateTime2.ToChineseDateTimeString() == expected);
+        }
+
+        [Fact(DisplayName = "转为yyyy-MM-dd日期格式-成功测试")]
+        public void ToDateStringShouldSuccess()
+        {
+            var datetime = Convert.ToDateTime("2019-12-11 23:10:10");
+            var expected = "2019-12-11";
+
+            DateTime? nullDateTime1 = null;
+            DateTime? nullDateTime2 = datetime;
+
+            Assert.True(datetime.ToDateString() == expected);
+            Assert.True(nullDateTime1.ToDateString() == string.Empty);
+            Assert.True(nullDateTime2.ToDateString() == expected);
+        }
+
+        [Theory(DisplayName = "转为友好的时间信息-成功测试")]
+        [InlineData("刚刚", 0)]
+        [InlineData("刚刚", 59)]
+        [InlineData("1分钟前", 60)]
+        [InlineData("1分钟前", 119)]
+        [InlineData("2分钟前", 60 * 2)]
+        [InlineData("2分钟前", 60 * 2 + 59)]
+        [InlineData("1小时前", 60 * 60)]
+        [InlineData("1小时前", 60 * 60 * 2 - 1)]
+        [InlineData("2小时前", 60 * 60 * 2)]
+        [InlineData("2小时前", 60 * 60 * 3 - 1)]
+        [InlineData("3小时前", 60 * 60 * 3)]
+        [InlineData("3小时前", 60 * 60 * 4 - 1)]
+        [InlineData("昨天", 60 * 60 * 24)]
+        [InlineData("1天前", 60 * 60 * 24 + 1)]
+        [InlineData("1天前", 60 * 60 * 24 * 2 - 1)]
+        [InlineData("2天前", 60 * 60 * 24 * 2)]
+        [InlineData("6天前", 60 * 60 * 24 * 6)]
+        [InlineData("6天前", 60 * 60 * 24 * 7 - 1)]
+        [InlineData("上周", 60 * 60 * 24 * 7)]
+        [InlineData("上周", 60 * 60 * 24 * 14 - 1)]
+        [InlineData("2周前", 60 * 60 * 24 * 14)]
+        [InlineData("2周前", 60 * 60 * 24 * 21 - 1)]
+        [InlineData("3周前", 60 * 60 * 24 * 21)]
+        [InlineData("3周前", 60 * 60 * 24 * 28 - 1)]
+        [InlineData("上个月前", 60 * 60 * 24 * 28)]
+        [InlineData("上个月前", 60 * 60 * 24 * 60 - 1)]
+        [InlineData("2个月前", 60 * 60 * 24 * 60)]
+        [InlineData("2个月前", 60 * 60 * 24 * 90 - 1)]
+        [InlineData("3个月前", 60 * 60 * 24 * 90)]
+        [InlineData("3个月前", 60 * 60 * 24 * 120 - 1)]
+        [InlineData("12个月前", 60 * 60 * 24 * 365 - 1)]
+        [InlineData("去年", 60 * 60 * 24 * 365)]
+        [InlineData("去年", 60 * 60 * 24 * 730 - 1)]
+        [InlineData("2年前", 60 * 60 * 24 * 365 * 2)]
+        [InlineData("2年前", 60 * 60 * 24 * 365 * 3 - 1)]
+        [InlineData("3年前", 60 * 60 * 24 * 365 * 3)]
+        public void ToFriendlyDateStringShouldSuccess(string friendlyInfo, int beSubtractSeconds)
+        {
+            var publishTime = new DateTime(2019, 12, 12, 18, 30, 0);
+            var now = new DateTime(2019, 12, 12, 18, 30, 0);
+
+            Assert.True(publishTime.AddSeconds(-1 * beSubtractSeconds).ToFriendlyDateString(now) == friendlyInfo);
         }
     }
 }
