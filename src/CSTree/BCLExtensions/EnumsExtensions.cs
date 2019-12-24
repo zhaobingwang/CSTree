ce86cd48ed27555d808d50bbdd475aa9de1e4da2
zhaobingwang <zhaobingwang@foxmali.com>
2019-12-24 20:34:10 +0800

    feat : 新增几个枚举扩展方法及其单元测试

---
+++ src/CSTree/BCLExtensions/EnumsExtensions.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Text;
+
+namespace CSTree.BCLExtensions
+{
+    public static class EnumsExtensions
+    {
+        /// <summary>
+        /// 将枚举值转为string
+        /// </summary>
+        /// <param name="source">枚举源</param>
+        /// <returns></returns>
+        public static string ToStringValue(this Enum source)
+        {
+            return Convert.ChangeType(source, typeof(int)).ToString();
+        }
+
+        /// <summary>
+        /// 将枚举值转为int32
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static int ToInt32Value(this Enum source)
+        {
+            return (int)Convert.ChangeType(source, typeof(int));
+        }
+
+        /// <summary>
+        /// 获取特性 (DisplayAttribute) 的名称
+        /// 如果未使用该特性，则返回枚举的名称
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static string GetDisplayName(this Enum source)
+        {
+            FieldInfo fieldInfo = source.GetType().GetField(source.ToString());
+            DisplayAttribute[] attrs =
+                fieldInfo.GetCustomAttributes(typeof(DisplayAttribute), false) as DisplayAttribute[];
+
+            return attrs.Length > 0 ? attrs[0].Name : source.ToString();
+        }
+
+        /// <summary>
+        /// 获取特性(DisplayAttribute)的描述
+        /// 如果未使用该特性，则返回枚举的名称
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static string GetDisplayDescription(this Enum source)
+        {
+            FieldInfo fieldInfo = source.GetType().GetField(source.ToString());
+            DisplayAttribute[] attrs =
+                fieldInfo.GetCustomAttributes(typeof(DisplayAttribute), false) as DisplayAttribute[];
+
+            return attrs.Length > 0 ? attrs[0].Description : source.ToString();
+        }
+
+        /// <summary>
+        /// 获取特性 (DescriptionAttribute)的说明
+        /// 如果未使用该特性，则返回枚举的名称。
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static string GetDescription(this Enum source)
+        {
+            FieldInfo fieldInfo = source.GetType().GetField(source.ToString());
+            DescriptionAttribute[] attrs =
+                fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false) as DescriptionAttribute[];
+
+            return attrs.Length > 0 ? attrs[0].Description : source.ToString();
+        }
+
+        /// <summary>
+        /// 获取特性
+        /// 不存在返回null
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static T GetCustomAttributes<T>(this Enum source) where T : Attribute
+        {
+            Type type = source.GetType();
+            MemberInfo[] memInfo = type.GetMember(source.ToString());
+            object[] attributes = memInfo[0].GetCustomAttributes(typeof(T), false);
+
+            return (attributes.Length > 0) ? (T)attributes[0] : null;
+        }
+    }
+
+}
---
+++ tests/CSTree.UnitTests/BCLExtensions/EnumsExtensionsTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using CSTree.BCLExtensions;
+using Xunit;
+
+namespace CSTree.UnitTests.BCLExtensions
+{
+    [Trait("BCL扩展方法", "enum扩展")]
+    public class EnumsExtensionsTest
+    {
+        [Fact(DisplayName = "将value转为string")]
+        public void ToStringValueShouldSuccess()
+        {
+            var stringVal1 = Order.ASC.ToStringValue();
+            var stringVal2 = Order.DESC.ToStringValue();
+
+            Assert.True(stringVal1 == "1");
+            Assert.True(stringVal2 == "2");
+        }
+
+        [Fact(DisplayName = "将value转为int32")]
+        public void ToInt32ValueShouldSuccess()
+        {
+            var int32Val1 = Order.ASC.ToInt32Value();
+            var int32Val2 = Order.DESC.ToInt32Value();
+
+            Assert.True(int32Val1 == 1);
+            Assert.True(int32Val2 == 2);
+        }
+
+        [Fact(DisplayName = "获取枚举的特性(DisplayAttribute)的名称")]
+        public void GetDisplayNameShouldSuccess()
+        {
+            Assert.True(Order.ASC.GetDisplayName() == "升序");
+        }
+
+        [Fact(DisplayName = "获取特性(DisplayAttribute)的描述")]
+        public void GetDisplayDescriptionShouldSuccess()
+        {
+            Assert.True(Order.ASC.GetDisplayDescription() == "按照升序排序");
+        }
+
+        [Fact(DisplayName = "获取特性(DescriptionAttribute)的说明")]
+        public void GetGetDescriptionShouldSuccess()
+        {
+            Assert.True(Order.ASC.GetDescription() == "asc");
+        }
+
+        [Fact(DisplayName = "获取特性")]
+        public void GetCustomAttributesShouldSuccess()
+        {
+            Assert.True(Order.ASC.GetCustomAttributes<TestAttribute>().DisplayName == "测试");
+            Assert.True(Order.ASC.GetCustomAttributes<ReadOnlyAttribute>() == null);
+        }
+
+        enum Order
+        {
+            /// <summary>
+            /// 升序
+            /// </summary>
+            [Description("asc")]
+            [Display(Name = "升序", Description = "按照升序排序")]
+            [Test(DisplayName = "测试")]
+            ASC = 1,
+            DESC = 2
+        }
+        class TestAttribute : System.Attribute
+        {
+            public string DisplayName { get; set; }
+        }
+    }
+
+
+}
